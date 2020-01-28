5f63d79b87393c0977ee2f6109b128c1ad3bcf3b
Roman Andriienko <Andriienkoroman@gmail.com>
2020-01-28 16:40:14 +0200

    squash add , sub , div , mult and sqrt commits
    create sub method
    
    create div method
    
    create mult method
    
    create sqrt method

---
+++ Calculate.java
@@ -0,0 +1,20 @@
+public class Calculate {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+    public int sqrt(int a) {
+        return Math.sqrt(a);
+    }
+}
